191048372385c31a4860dd4e0c2f6a18b745e87b
Scott Fones <scott.fones@gmail.com>
2021-10-05 12:27:54 -0400

    removed all tests and associated code except testFromRomanInvalidForm()

--- src/test/java/roman/FromRomanInvalid.java
+++ src/test/java/roman/FromRomanInvalid.java
@@ -11,11 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class FromRomanInvalid {
     private RomanConverter converter;
 
-    static String[] numeralOnes = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-    static String[] numeralTens = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-    static String[] numeralHunds = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-    static String[] numeralThous = { "M", "MM", "MMM" };
-
     @BeforeEach
     public void setUp() {
         converter = TestImplValue.getImplValue();
@@ -25,25 +20,5 @@ public class FromRomanInvalid {
     @ValueSource(strings = { "IIIIIV", "VX", "LC", "DM", "VL", "XD", "IM" })
     public void testFromRomanInvalidForm(String invNumeral) {
         assertThrows(IllegalArgumentException.class, () -> converter.fromRoman(invNumeral));
-    }
-
-    @Test
-    public void testFromRomanEmpty() {
-        assertEquals(0, converter.fromRoman(""));
-    }
-
-    @Test
-    public void testFromRomanInvalidDoubleUnits() {
-        for (int i = 4; i < 9; i++) {
-            String onesNum = numeralOnes[i] + numeralOnes[i];
-            assertThrows(IllegalArgumentException.class, () -> converter.fromRoman(onesNum));
-
-            String tensNum = numeralTens[i] + numeralTens[i];
-            assertThrows(IllegalArgumentException.class, () -> converter.fromRoman(tensNum));
-
-            String hundsNum = numeralHunds[i] + numeralHunds[i];
-            assertThrows(IllegalArgumentException.class, () -> converter.fromRoman(hundsNum));
-
-        }
     }
 }
